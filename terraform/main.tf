ab59c23c31d680ff48c0c4bcc71aed9549dbe8f4
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2025-05-29 11:21:53 -0700

    Add password policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -626,3 +626,17 @@ resource "aws_iam_user" "data_analysts_2" {
 resource "aws_iam_user" "data_analysts_3" {
   name = "data_analyst_3"
 }
+
+
+
+# IAM Password Policy
+resource "aws_iam_account_password_policy" "password_policy" {
+  minimum_password_length        = 12
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  max_password_age = 90 # 90 day password age
+  password_reuse_prevention = 5 # cannot resuse last 5 passwords
+}
